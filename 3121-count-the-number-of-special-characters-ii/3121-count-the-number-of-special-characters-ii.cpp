10863d1eb15beb83ddbecdf8f8ba2c2c6f07addd
Gaurav Mani <142533365+gauravmani007@users.noreply.github.com>
2026-05-28 01:54:17 +0530

    Time: 96 ms (23.31%), Space: 25.5 MB (23.39%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,32 +1,77 @@
+
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
         int n = word.size();
-
-        vector<int>uppercase(26, -1);
-        vector<int>lowercase(26, -1);
-
+        unordered_map<char, int>mp;
         int count = 0;
 
-        for(int i = 0; i<n; i++){
-            if(word[i] >= 'a' && word[i] <= 'z'){
-                lowercase[word[i]-'a'] = i;
+        for(int i=0; i<n; i++){
+            if(islower(word[i])){
+                mp[word[i]] = i;
             }
             else{
-                if(uppercase[word[i]-'A'] < 0){
-                uppercase[word[i] - 'A'] = i;
+                if(mp.find(word[i]) == mp.end()){
+                mp[word[i]] = i;
                 }
             }
         }
 
-        for(int i=0; i<26; i++){
-            if(lowercase[i] != -1 && uppercase[i] != -1 && lowercase[i] < uppercase[i]){
-                count++;
-            }
-            else{
-                continue;
+        for(auto it : mp){
+            if(islower(it.first) && mp.find(toupper(it.first)) != mp.end()){
+                if(it.second < mp[toupper(it.first)]){
+                    count++;
+                }
             }
         }
+
     return count;
-    }
+  }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int numberOfSpecialChars(string word) {
+//         int n = word.size();
+
+//         vector<int>uppercase(26, -1);
+//         vector<int>lowercase(26, -1);
+
+//         int count = 0;
+
+//         for(int i = 0; i<n; i++){
+//             if(word[i] >= 'a' && word[i] <= 'z'){
+//                 lowercase[word[i]-'a'] = i;
+//             }
+//             else{
+//                 if(uppercase[word[i]-'A'] < 0){
+//                 uppercase[word[i] - 'A'] = i;
+//                 }
+//             }
+//         }
+
+//         for(int i=0; i<26; i++){
+//             if(lowercase[i] != -1 && uppercase[i] != -1 && lowercase[i] < uppercase[i]){
+//                 count++;
+//             }
+//             else{
+//                 continue;
+//             }
+//         }
+//     return count;
+//     }
+// };
