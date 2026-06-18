4ef613fcb14db5b2f6c18605bc35e5c49719e4fd
Gaurav Mani <142533365+gauravmani007@users.noreply.github.com>
2026-06-18 22:24:27 +0530

    Time: 7 ms (33.11%), Space: 39.9 MB (43.02%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -2,27 +2,47 @@ class Solution {
 public:
     string processStr(string s) {
         int n = s.length();
-     
-        deque<char>q;
-        string result; 
-
+        string res;
         for(int i=0; i<n; i++){
-            if(s[i] != '*' && s[i] != '#' && s[i] != '%'){
-                q.push_back(s[i]);
-            }else if(s[i] == '*' && !q.empty()){
-                q.pop_back();
-            }else if(s[i] == '%' && !q.empty()){
-                reverse(q.begin(), q.end());
-            }else if(s[i] == '#' && !q.empty()){
-                deque<char>temp = q;
-                q.insert(q.end(), temp.begin(), temp.end());
+            if(s[i] == '*'){
+                if(res.length() != 0){
+                    res.pop_back();
+                }
+            }
+            else if(s[i] == '#'){
+                res += res;
+            }
+            else if(s[i]== '%'){
+                reverse(res.begin(), res.end());
+            }
+
+            else if(s[i] >= 'a' && s[i] <= 'z'){
+                res.push_back(s[i]);
             }
         }
 
-        for(int i=0; i<q.size(); i++){
-            result.push_back(q[i]);
-        }
-
-        return result;
+    return res;
     }
 };
+        // deque<char>q;
+        // string result; 
+
+        // for(int i=0; i<n; i++){
+        //     if(s[i] != '*' && s[i] != '#' && s[i] != '%'){
+        //         q.push_back(s[i]);
+        //     }else if(s[i] == '*' && !q.empty()){
+        //         q.pop_back();
+        //     }else if(s[i] == '%' && !q.empty()){
+        //         reverse(q.begin(), q.end());
+        //     }else if(s[i] == '#' && !q.empty()){
+        //         deque<char>temp = q;
+        //         q.insert(q.end(), temp.begin(), temp.end());
+        //     }
+        // }
+
+        // for(int i=0; i<q.size(); i++){
+        //     result.push_back(q[i]);
+        // }
+
+        // return result;
+    
